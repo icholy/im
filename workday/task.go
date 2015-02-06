5295e2e065118b21969cf3c1b4ca92e5e5346f44
Ilia Choly <ilia.choly@gmail.com>
2015-02-06 12:44:14 -0500

    add tag parsing

--- workday/task.go
+++ workday/task.go
@@ -1,14 +1,31 @@
 package workday
 
-import "time"
+import (
+	"regexp"
+	"time"
+)
 
 type Task struct {
 	Time time.Time
 	Desc string
+	Tags []string
+}
+
+func findTags(s string) []string {
+	r := regexp.MustCompile(`@\w+`)
+	tags := r.FindAllString(s, -1)
+	for i, tag := range tags {
+		tags[i] = tag[1:]
+	}
+	return tags
 }
 
 func AddTask(desc string) error {
-	t := &Task{time.Now(), desc}
+	t := &Task{
+		Time: time.Now(),
+		Desc: desc,
+		Tags: findTags(desc),
+	}
 	d, err := LoadDay(t.Time)
 	if err != nil {
 		return err
