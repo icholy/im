42cb22b5be4659f30d23f0dfdd10007359517f55
Ilia Choly <ilia.choly@gmail.com>
2015-05-04 14:34:17 -0400

    remove tags

--- workday/task.go
+++ workday/task.go
@@ -1,30 +1,16 @@
 package workday
 
-import (
-	"regexp"
-	"time"
-)
+import "time"
 
 type Task struct {
 	Time time.Time
 	Desc string
-	Tags []string
-}
-
-func findTags(s string) []string {
-	r := regexp.MustCompile(`@\w+`)
-	tags := r.FindAllString(s, -1)
-	for i, tag := range tags {
-		tags[i] = tag[1:]
-	}
-	return tags
 }
 
 func AddTask(desc string) error {
 	t := &Task{
 		Time: time.Now(),
 		Desc: desc,
-		Tags: findTags(desc),
 	}
 	d, err := LoadDay(t.Time)
 	if err != nil {
